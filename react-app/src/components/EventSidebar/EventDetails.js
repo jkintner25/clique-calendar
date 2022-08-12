f30487f11af983289c7a4876f62560eabb4658de
jkintner25 <jkintner25@gmail.com>
2022-08-12 15:06:02 -0500

    fixed incorrect date format for selected event in event details pane

--- react-app/src/components/EventSidebar/EventDetails.js
+++ react-app/src/components/EventSidebar/EventDetails.js
@@ -25,28 +25,39 @@ hyphens: auto;
 `
 
 function EventDetails() {
-    const selectedEvent = useSelector(state=>state.selectedEvent)
+    const selectedEvent = useSelector(state => state.selectedEvent)
     const [event, setEvent] = useState(null)
+    const [selectedStartDate, setSelectedStartDate] = useState(null)
+    const [selectedEndDate, setSelectedEndDate] = useState(null)
+    const [newEvent, setNewEvent] = useState(null)
     const [updatedEvent, setUpdatedEvent] = useState(false)
 
     useEffect(()=>{
-        if(selectedEvent === {}) return;
+        if(selectedEvent === null || Object.values(selectedEvent).length < 1) return;
         setEvent(selectedEvent)
+        setUpdatedEvent(false)
     }, [selectedEvent])
 
-    useEffect(()=>{
-        if(!event) return;
-        event.startDate = dayjs(event.startDate).format('ddd, MMM D, h:mm a')
-        event.endDate = dayjs(event.endDate).format('ddd, MMM D, h:mm a')
+    useEffect(() => {
+        if(event === null) return;
+        setSelectedStartDate(dayjs(event.startDate).format('ddd, MMM D, h:mm a'))
+        setSelectedEndDate(dayjs(event.endDate).format('ddd, MMM D, h:mm a'))
+        setNewEvent(event)
+        setUpdatedEvent(false)
+    }, [selectedEvent])
+
+    useEffect(() => {
+        if (newEvent === null) return;
         setUpdatedEvent(true)
     }, [event])
+
 
     return (
         <EventDetailsContainer>
             {event && updatedEvent ? <ContentContainer>
                 <h2>{event.title}</h2>
-                <p>Start: {event.startDate}</p>
-                <p>End: {event.endDate}</p>
+                {selectedStartDate && <p>Start: {selectedStartDate}</p>}
+                {selectedEndDate && <p>End: {selectedEndDate}</p>}
                 <EventDetailsP>Details: {event.description}</EventDetailsP>
             </ContentContainer> : <p>Select an event...</p>}
         </EventDetailsContainer>
